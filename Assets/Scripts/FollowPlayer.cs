979299c888c71c0a75c1abc591ddaa6d0eb52ec1
Hirayama_Evan <hirae675@hsd.k12.or.us>
2025-03-12 10:56:51 -0700

    prototype 1st person attempt

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 6, -7);
+    public Vector3 offset = new Vector3(0, 2, 2);
+    private float turnSpeed = 20.0f;
+    private float horizontalInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,6 +16,9 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+
         transform.position = player.transform.position + offset;
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
